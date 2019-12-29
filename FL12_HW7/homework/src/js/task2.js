accda20afa168cca5031650549787a570f6d46ab
Ilya Protsenko <55619062+ilyaprotsenk0@users.noreply.github.com>
2019-12-29 13:45:38 +0200

    Add files via upload

--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -1,22 +1,22 @@
 let startGame = confirm('Do you want to play a game?');
 let totalPrize = 0;
 let roundPrize = 0;
-let a = 0;
-let gameRound = 1;
+let roundRatio = 0;
+let currentRound = 1;
 
-while (gameRound > 0) {
+while (currentRound > 0) {
 
     if (startGame === false) {
         alert('You did not become a billionaire, but can');
-        gameRound = 0;
+        currentRound = 0;
     } else {
-        let randNum = Math.floor(Math.random() * (9 + 4*a));
-        let possiblePrize = 100 * Math.pow(2, a);
+        let randNum = Math.floor(Math.random() * (9 + 4*roundRatio));
+        let possiblePrize = 100 * Math.pow(2, roundRatio);
         let attemts = 3;
         let attemtsCheck = false;
 
         for (let i = 0; i < 3; i++) {
-            let userNum = prompt('Choose a roulete pocket number from 0 to ' + (8 + 4*a) + 
+            let userNum = prompt('Choose a roulete pocket number from 0 to ' + (8 + 4*roundRatio) + 
                                  '\nAttempts left: ' + (attemts - i) +
                                  '\nTotal prize: ' + totalPrize + 
                                  '\nPossible prize on current attempt: ' + possiblePrize);
@@ -35,23 +35,23 @@ while (gameRound > 0) {
            let playAgain = confirm('Congratulation, you won! Your prize is: '+totalPrize+'. Do you want to continue?');
 
             if (playAgain === true) {
-                gameRound++;
-                a++;
+                currentRound++;
+                roundRatio++;
             } else {
                 alert("U didn't become a millionare but can!");
-                gameRound = 0;
+                currentRound = 0;
             }
         } else {
             alert('Thank you for your participation. Your prize is: ' + totalPrize);
             let playAgain = confirm('Do you want to play again?');
 
             if (playAgain === true) {
-                gameRound = 1;
-                a = 0;
+                currentRound = 1;
+                roundRatio = 0;
                 totalPrize = 0;
             } else {
                 alert("U didn't become a millionare but can!");
-                gameRound = 0;
+                currentRound = 0;
             }
         }
     }
